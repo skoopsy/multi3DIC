4e4ba1e67f1d90e9cc0ad548596179d9f4f9a855
dave <dave@daves-MacBook-Pro.local>
2024-08-08 11:16:19 +0100

    pepify, comments, clean, minor refactor

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from scipy.spatial import Delaunay
 import sys
 sys.path.append("..")
-import os
+
 
 class DICDataset:
     """ Class to hold data from a stereo DIC dataset """
@@ -18,55 +17,82 @@ class DICDataset:
         self.z_filtered = None
         self.strain = None
         self.strain_filtered = None
-        self.trisurface = None # Depreciate
+        self.trisurface = None  # Depreciate
         self.simplices = None
         self.simplices_filtered = None
 
+
 def import_data(path, import_type):
     """
-    Loads a vector field file into a
-    DICDataset object.
+    Loads a vector field file into a DICDataset object. The vector field file is assumed
+    to have been produced by DIC software producing xyz coordinates along with the
+    associated strain values. This import currently imports a single timestep.
+
+    :param  path :str
+    :param  import_type :str
+    :return: None
     """
     csv_df = pd.read_csv(path)
 
     # Instantiate class to store data
-    data = DICDataset()
+    dic_data = DICDataset()
 
     if import_type == 'LAVision':
         # Put data in numpy arrays
-        data.x = csv_df['x[mm]'].to_numpy()
-        data.y = csv_df['y[mm]'].to_numpy()
-        data.z = csv_df['z[mm]'].to_numpy()
-        data.strain = csv_df['Maximum normal strain - RC[S]'].to_numpy()
+        dic_data.x = csv_df['x[mm]'].to_numpy()
+        dic_data.y = csv_df['y[mm]'].to_numpy()
+        dic_data.z = csv_df['z[mm]'].to_numpy()
+        dic_data.strain = csv_df['Maximum normal strain - RC[S]'].to_numpy()
 
-    return data
+    return dic_data
 
-def create_delaunay_mesh(DICDataset):
+
+def filter_strain0_data(DICDataset):
+    """
+    Fixes Region of Interest (ROI) issue where the mesh is the full
+    resolution of the camera which captures noise. This removes
+    data points which correspond to a zero strain reading.
+
+    This could bite back if the ROI has areas of 0 strain, but
+    due to noise the measurements are unlikely to == 0.
+    """
+    mask = DICDataset.strain != 0
+    DICDataset.x_filtered = DICDataset.x[mask]
+    DICDataset.y_filtered = DICDataset.y[mask]
+    DICDataset.z_filtered = DICDataset.z[mask]
+    DICDataset.strain_filtered = DICDataset.strain[mask]
+
+    # Create mapping from old indices to new
+    # index_map = np.zeros_like(mask, dtype=int)
+    # index_map[mask] = np.arange(len(DICDataset.x_filtered))
+
+    # Filter simplices to remove any that contain removed points
+    # DICDataset.simplices_filtered = []
+    # for simplex in DICDataset.simplices:
+    #    if mask[simplex].all():  # Only keep simplices where all points are kept
+    #        DICDataset.simplices_filtered.append(index_map[simplex])
+
+    # DICDataset.simplices_filtered = np.array(DICDataset.simplices_filtered)
+
+    return None
+
+
+def compute_delaunay_mesh(DICDataset, filtered: bool = False):
     """
     Construct a Delaunay triangular mesh from a DICDataset with x,y,z
     coordinates as an attribute in the DICDataset object.
 
-    :param DICDataset:
+    :param DICDataset :DICDataset object
     :return: None
     """
-    # Create array of 2d points (xy)
-    points2D = np.vstack([DICDataset.x,DICDataset.y]).T
+    if filtered:
+        points_2d = np.vstack((DICDataset.x_filtered, DICDataset.y_filtered)).T
+    else:
+        points_2d = np.vstack((DICDataset.x, DICDataset.y)).T
 
-    # Use Delaunay meshing to connect 2d points
-    tri = Delaunay(points2D)
-    DICDataset.simplices = tri.simplices
+    delaunay = Delaunay(points_2d)
+    DICDataset.simplices_filtered = delaunay.simplices
 
-    return None
-
-def plot_delaunay_mesh(x,y,z,simplices, z_scale=1):
-    # Just for testing, using go for strain map.
-    fig = ff.create_trisurf(x=x, y=y, z=z,
-                            simplices=simplices,
-                            title="DIC Surface Map",
-                            aspectratio=dict(x=1, y=1, z=z_scale))
-    fig.show()
-
-    return None
 
 def plot_delaunay_mesh_strain(x, y, z, simplices, strain, z_scale=1):
     # Create trisurface plot
@@ -99,41 +125,6 @@ def plot_delaunay_mesh_strain(x, y, z, simplices, strain, z_scale=1):
     fig.show()
 
     return None
-def filter_strain0_data(DICDataset):
-    """
-    Fixes Region of interest issue where the mesh is the full
-    resolution of the camera which captures noise. This removes
-    data points which correspond to a zero strain reading.
-
-    This could bite back if the ROI has areas of 0 strain but
-    due to measurement noise I do not think the values will ever == 0.
-    """
-
-    # Filter out points where strain = 0
-    mask = DICDataset.strain != 0
-    DICDataset.x_filtered = DICDataset.x[mask]
-    DICDataset.y_filtered = DICDataset.y[mask]
-    DICDataset.z_filtered = DICDataset.z[mask]
-    DICDataset.strain_filtered = DICDataset.strain[mask]
-
-    # Create mapping from old indices to new
-    #index_map = np.zeros_like(mask, dtype=int)
-    #index_map[mask] = np.arange(len(DICDataset.x_filtered))
-
-    # Filter simplices to remove any that contain removed points
-    #DICDataset.simplices_filtered = []
-    #for simplex in DICDataset.simplices:
-    #    if mask[simplex].all():  # Only keep simplices where all points are kept
-    #        DICDataset.simplices_filtered.append(index_map[simplex])
-
-    #DICDataset.simplices_filtered = np.array(DICDataset.simplices_filtered)
-
-    return None
-
-    points_2d = np.vstack((DICDataset.x_filtered, DICDataset.y_filtered)).T
-    delaunay = Delaunay(points_2d)
-    DICDataset.simplices_filtered = delaunay.simplices
-
 def combine_stereo_pairs(meshes):
     all_data = DICDataset
     vertext_offset = 0
@@ -161,7 +152,8 @@ if __name__ == '__main__':
 
     all_data = combine_stereo_pairs([data1, data2, data3])
     filter_strain0_data(all_data)
-    compute_delaunay_mesh(all_data)
+    compute_delaunay_mesh(all_data, filtered=True)
+
     plot_delaunay_mesh_strain(x=all_data.x_filtered, y=all_data.y_filtered,
                               z=all_data.z_filtered,
                               simplices=all_data.simplices_filtered,
