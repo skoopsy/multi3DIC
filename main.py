2c1f86035a42fb27254463d12e5f2abeeead585e
dave <dave@daves-MBP.lan>
2024-08-09 20:18:22 +0100

    merging and filtering done

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ class Config:
     debugging = True
     print_sep = str(50 * "=")
 
-class ImportedDICDataset:
-    """ Imported dataset class"""
-    def __init__(self):
-        self.x = []
-        self.y = []
-        self.z = []
-        self.strain = []
 
 class DICDataset:
     """ Main dataset class"""
@@ -30,12 +23,12 @@ conf = Config()
 def import_and_combine_timestep(paths: list[str], import_type: str, config: Config()):
 
     # Initialise
-    importedData = ImportedDICDataset()
+    imported_data = DICDataset()
 
-    importedData.x = []
-    importedData.y = []
-    importedData.z = []
-    importedData.strain = []
+    imported_data.x = []
+    imported_data.y = []
+    imported_data.z = []
+    imported_data.strain = []
 
     # Iterate through files
     for path in paths:
@@ -47,36 +40,73 @@ def import_and_combine_timestep(paths: list[str], import_type: str, config: Conf
             print(f"Loaded: {path}")
             print(f"Shape: {csv_df.shape}")
             print(f"Columns Found: {csv_df.columns}")
-            print(config.print_sep)
 
         if import_type == 'LAVision':
 
             # Check columns and import data
             expected_cols = ['x[mm]', 'y[mm]', 'z[mm]', 'Maximum normal strain - RC[S]']
             if all(col in csv_df.columns for col in expected_cols):
-                importedData.x.append(csv_df["x[mm]"].to_numpy())
-                importedData.y.append(csv_df["y[mm]"].to_numpy())
-                importedData.z.append(csv_df["z[mm]"].to_numpy())
-                importedData.strain.append(csv_df[("Maximum normal strain - RC["
+                imported_data.x.append(csv_df["x[mm]"].to_numpy())
+                imported_data.y.append(csv_df["y[mm]"].to_numpy())
+                imported_data.z.append(csv_df["z[mm]"].to_numpy())
+                imported_data.strain.append(csv_df[("Maximum normal strain - RC["
                                                  "S]")].to_numpy())
             else:
                 print(f"Warning: Missing columns in {path}. Columns expected: "
                       f"{expected_cols}")
 
     break_point = 0
-    return importedData
+    return imported_data
 
 
 def merge_datasets(ImportedDICDataset):
-    return None
+    merged_data = DICDataset()
+
+    merged_data.x = np.concatenate(imported_data.x, axis=0)
+    merged_data.y = np.concatenate(imported_data.y, axis=0)
+    merged_data.z = np.concatenate(imported_data.z, axis=0)
+    merged_data.strain = np.concatenate(imported_data.strain, axis=0)
+
+    if conf.debugging:
+        i = len(imported_data.x)
+        print(conf.print_sep)
+        print(f"{i} datasets found and merged.")
+
+    break_point = 0
+    return merged_data
+
+
+def filter_strain0(DICDataset):
+   if conf.debugging:
+       print(conf.print_sep)
+       print("Filtering...")
+
+   # Create boolean mask based on strain values
+   mask = DICDataset.strain != 0
+
+   # Filter everything with mask
+   DICDataset.x = DICDataset.x[mask]
+   DICDataset.y = DICDataset.y[mask]
+   DICDataset.z = DICDataset.z[mask]
+   DICDataset.strain = DICDataset.strain[mask]
+
+   if conf.debugging:
+       total_entries = len(mask)
+       entries_removed = total_entries - np.count_nonzero(mask)
+       print(f"Entries removed: {entries_removed} of {total_entries}")
+
+
+   return None
+
 
 file_paths = ['test_files/vector_field_export_cam1-2-0001.csv',
               'test_files/vector_field_export_cam2-3-0001.csv',
               'test_files/vector_field_export_cam3-4-0001.csv',]
 
-importedData = import_and_combine_timestep(paths=file_paths,
-                                           import_type='LAVision',
-                                           config=conf)
-data = merge_datasets(importedData)
+imported_data = import_and_combine_timestep(paths=file_paths,
+                                            import_type='LAVision',
+                                            config=conf)
+data = merge_datasets(imported_data)
+filter_strain0(data)
 
 break_point=0
