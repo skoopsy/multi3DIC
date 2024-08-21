8c41854eecd80fbba2e0859f7a8fd7ffe8f9e54e
dave <dave@daves-mbp.home>
2024-08-21 19:15:55 +0100

    prototype combining 3 datasets in plotly

--- exp_plotly_trisurfs.py
+++ exp_plotly_trisurfs.py
@@ -19,6 +19,30 @@ simplices = tri.simplices
 
 fig = ff.create_trisurf(x=x, y=y, z=z,
                          simplices=simplices,
-                         title="Torus", aspectratio=dict(x=1, y=1, z=0.3))
+                         title="Torus", aspectratio=dict(x=2, y=2, z=0.6))
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    margin=dict(
+        l=10,
+        r=10,
+        b=10,
+        t=30,
+        pad=4
+    )
+)
+
 fig.show()
+fig.write_html('plots/torus.html', full_html=False, include_plotlyjs='cdn')
+
+import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_trisurf(x, y, z, triangles=simplices, edgecolors='k')
+ax.set_box_aspect([1,1,0.25])
+plt.show()
+
 
