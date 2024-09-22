c01820e1ed410e38fe2e5906e52a76a7ab20e48d
Dave O <38195019+skoopsy@users.noreply.github.com>
2024-09-22 08:34:17 -0400

    Feature/04 combined datasets (#4)
    * gitignore
    
    * multi data import working
    
    * merging and filtering done
    
    * compute basic delaunay mesh and print post mass filter
    
    * added interactive plot
    
    * added plot write to html
    
    * added html save for visualisation
    
    ---------
    
    Co-authored-by: dave <dave@daves-MacBook-Pro.local>
    Co-authored-by: dave <dave@daves-MBP.lan>

--- main.py
+++ main.py
@@ -1,154 +1,164 @@
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
-import plotly.graph_objects as go
-from scipy.spatial import Delaunay
-import sys
-sys.path.append("..")
+from scipy.spatial import Delaunay # compute_delaunay_mesh()
+import plotly.graph_objects as go # plot_mesh_interactive()
+
+
+class Config:
+    """ General configuration class for prototyping"""
+    debugging = True
+    print_sep = str(50 * "=")
 
 
 class DICDataset:
-    """ Class to hold data from a stereo DIC dataset """
+    """ Main dataset class"""
     def __init__(self):
         self.x = None
-        self.x_filtered = None
         self.y = None
-        self.y_filtered = None
         self.z = None
-        self.z_filtered = None
         self.strain = None
-        self.strain_filtered = None
-        self.trisurface = None  # Depreciate
         self.simplices = None
-        self.simplices_filtered = None
 
 
-def import_data(path, import_type):
-    """
-    Loads a vector field file into a DICDataset object. The vector field file is assumed
-    to have been produced by DIC software producing xyz coordinates along with the
-    associated strain values. This import currently imports a single timestep.
-
-    :param  path :string
-    :param  import_type :string
-    :return: None
-    """
-    csv_df = pd.read_csv(path)
-
-    # Instantiate class to store data
-    dic_data = DICDataset()
-
-    if import_type == 'LAVision':
-        # Put data in numpy arrays
-        dic_data.x = csv_df['x[mm]'].to_numpy()
-        dic_data.y = csv_df['y[mm]'].to_numpy()
-        dic_data.z = csv_df['z[mm]'].to_numpy()
-        dic_data.strain = csv_df['Maximum normal strain - RC[S]'].to_numpy()
-
-    return dic_data
+conf = Config()
 
 
-def create_delaunay_mesh(DICDataset):
-    """
-    Construct a Delaunay triangular mesh from a DICDataset with x,y,z
-    coordinates as an attribute in the DICDataset object.
+def import_dic_data(paths: list[str], import_type: str, config: Config()):
 
-    :param DICDataset :DICDataset object
-    :return: None
-    """
-    # Create array of 2d points (xy)
-    points_2d = np.vstack([DICDataset.x, DICDataset.y]).T
+    # Initialise
+    imported_data = DICDataset()
 
-    # Use Delaunay meshing to connect 2d points
+    imported_data.x = []
+    imported_data.y = []
+    imported_data.z = []
+    imported_data.strain = []
+
+    # Iterate through files
+    for path in paths:
+
+        csv_df = pd.read_csv(path)
+
+        if config.debugging:
+            print(config.print_sep)
+            print(f"Loaded: {path}")
+            print(f"Shape: {csv_df.shape}")
+            print(f"Columns Found: {csv_df.columns}")
+
+        if import_type == 'LAVision':
+
+            # Check columns and place data into lists of separate numpy arrays
+            expected_cols = ['x[mm]', 'y[mm]', 'z[mm]', 'Maximum normal strain - RC[S]']
+            if all(col in csv_df.columns for col in expected_cols):
+                imported_data.x.append(csv_df["x[mm]"].to_numpy())
+                imported_data.y.append(csv_df["y[mm]"].to_numpy())
+                imported_data.z.append(csv_df["z[mm]"].to_numpy())
+                imported_data.strain.append(csv_df["Maximum normal strain - RC[""S]"
+                                                   ].to_numpy())
+            else:
+                print(f"Warning: Missing columns in {path}. Columns expected: "
+                      f"{expected_cols}")
+
+    return imported_data
+
+
+def merge_datasets(imported_data: DICDataset):
+    merged_data = DICDataset()
+
+    merged_data.x = np.concatenate(imported_data.x, axis=0)
+    merged_data.y = np.concatenate(imported_data.y, axis=0)
+    merged_data.z = np.concatenate(imported_data.z, axis=0)
+    merged_data.strain = np.concatenate(imported_data.strain, axis=0)
+
+    if conf.debugging:
+        i = len(imported_data.x)
+        print(conf.print_sep)
+        print(f"{i} datasets found and merged.")
+
+    return merged_data
+
+
+def filter_strain0(DICDataset):
+    if conf.debugging:
+        print(conf.print_sep)
+        print("Filtering...")
+
+    # Create boolean mask based on strain values
+    mask = DICDataset.strain != 0
+
+    # Filter everything with mask
+    DICDataset.x = DICDataset.x[mask]
+    DICDataset.y = DICDataset.y[mask]
+    DICDataset.z = DICDataset.z[mask]
+    DICDataset.strain = DICDataset.strain[mask]
+
+    if conf.debugging:
+        total_entries = len(mask)
+        entries_removed = total_entries - np.count_nonzero(mask)
+        print(f"Entries removed: {entries_removed} of {total_entries}")
+        print(f"Entries remaining: {total_entries-entries_removed}")
+
+    return None
+
+
+def compute_delaunay_mesh(DICDataset):
+    z = DICDataset.z
+    points_2d = np.column_stack((DICDataset.x, DICDataset.y))
     tri = Delaunay(points_2d)
+
     DICDataset.simplices = tri.simplices
 
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(points_2d[:, 0], points_2d[:, 1], z, triangles=tri.simplices,
+                    cmap=plt.cm.Spectral)
+
+    plt.show()
+    
     return None
 
 
-def plot_delaunay_mesh(x, y, z, simplices, z_scale=1):
-    # Just for testing, using go for strain map.
-    fig = ff.create_trisurf(x=x, y=y, z=z,
-                            simplices=simplices,
-                            title="DIC Surface Map",
-                            aspectratio=dict(x=1, y=1, z=z_scale))
-    fig.show()
-
-    return None
-
-
-def plot_delaunay_mesh_strain(x, y, z, simplices, strain, z_scale=1):
-    # Create trisurface plot
-    mesh = go.Mesh3d(
-        x=x,
-        y=y,
-        z=z,
-        i=simplices[:, 0],
-        j=simplices[:, 1],
-        k=simplices[:, 2],
-        intensity=strain,
-        colorscale='Viridis',
-        colorbar=dict(title='Strain'),
-        flatshading=True
-    )
-
-    # Set up layout with scaling
-    layout = go.Layout(
-        scene=dict(
-            aspectratio=dict(x=1, y=1, z=z_scale),
-            xaxis=dict(title='X [mm]'),
-            yaxis=dict(title='Y [mm]'),
-            zaxis=dict(title='Z [mm]'),
-        ),
-        title="DIC Surface Map"
-    )
-
-    # Create the figure and plot
-    fig = go.Figure(data=[mesh], layout=layout)
-    fig.show()
-
-    return None
-
-def filter_strain0_data(DICDataset):
+def plot_mesh_interactive(DICDataset, plot_save_path):
     """
-    Fixes Region of Interest (ROI) issue where the mesh is the full
-    resolution of the camera which captures noise. This removes
-    data points which correspond to a zero strain reading.
+    Using plotly go.Mesh3D to make 3d interactive plot specified
+    with custom triangles
 
-    This could bite back if the ROI has areas of 0 strain, but
-    due to noise the measurements are unlikely to == 0.
+    Note: If you only specify points then it will automatically perform
+    a delaunay tessilation, we do not want that due to the filtering, in
+    this case we specify i,j, and k.
     """
 
-    # Filter out points where strain = 0
-    mask = DICDataset.strain != 0
-    DICDataset.x_filtered = DICDataset.x[mask]
-    DICDataset.y_filtered = DICDataset.y[mask]
-    DICDataset.z_filtered = DICDataset.z[mask]
-    DICDataset.strain_filtered = DICDataset.strain[mask]
-
-    # Create mapping from old indices to new
-    index_map = np.zeros_like(mask, dtype=int)
-    index_map[mask] = np.arange(len(DICDataset.x_filtered))
-
-    # Filter simplices to remove any that contain removed points
-    DICDataset.simplices_filtered = []
-    for simplex in DICDataset.simplices:
-        if mask[simplex].all():  # Only keep simplices where all points are kept
-            DICDataset.simplices_filtered.append(index_map[simplex])
-
-    DICDataset.simplices_filtered = np.array(DICDataset.simplices_filtered)
-
+    fig = go.Figure(data=go.Mesh3d(x=DICDataset.x,
+                                   y=DICDataset.y,
+                                   z=DICDataset.z,
+                                   i=DICDataset.simplices[:,0],
+                                   j=DICDataset.simplices[:,1],
+                                   k=DICDataset.simplices[:,2],
+                                   intensity=DICDataset.strain,
+                                   colorbar=dict(title='Strain'),
+                                   opacity=0.9),
+                    layout=go.Layout(title="DIC Surface Map (Automatic Delaunay "
+                                           "Tessilation (Plotly)",
+                                     scene=dict(
+                                         xaxis=dict(title='X [mm]'),
+                                         yaxis=dict(title='Y [mm]'),
+                                         zaxis=dict(title='Z [mm]'),
+                                     )
+                                     ))
+    fig.show()
+    fig.write_html(plot_save_path)
     return None
 
 
-if __name__ == '__main__':
+file_paths = ['test_files/vector_field_export_cam1-2-0001.csv',
+              'test_files/vector_field_export_cam2-3-0001.csv',
+              'test_files/vector_field_export_cam3-4-0001.csv',]
 
-    data = import_data(path='test_files/vector_field_export_cam1-2-0001.csv',
-                       import_type='LAVision')
-    create_delaunay_mesh(data)
-    filter_strain0_data(data)
-
-    plot_delaunay_mesh_strain(x=data.x_filtered, y=data.y_filtered,
-                              z=data.z_filtered,
-                              simplices=data.simplices_filtered,
-                              strain=data.strain_filtered)
+imported_data = import_dic_data(paths=file_paths,
+                                            import_type='LAVision',
+                                            config=conf)
+data = merge_datasets(imported_data)
+filter_strain0(data)
+compute_delaunay_mesh(data)
+plot_save_path = "plots/3d_plot_1timestep-combine-filter-delaunay-plot.html"
+plot_mesh_interactive(data, plot_save_path)
